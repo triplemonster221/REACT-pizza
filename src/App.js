367b684370107188eb437ab5ddce3d79cb8a35b4
triplemonster221 <Boorder1993@yandex.ru>
2026-04-09 10:41:59 +0300

    create components Header Categories Sort PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from "./components/categories";
+import Header from "./components/header";
+import PizzaBlock from "./components/pizza-block";
+import Sort from "./components/sort";
+import "./scss/app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
